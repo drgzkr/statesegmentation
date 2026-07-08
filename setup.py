67d58e77375a1dba9ffb1d9cf4554d9f3e63bc1e
drgzkr <60198204+drgzkr@users.noreply.github.com>
2026-07-08 18:46:49 +0200

    Bump version from 0.0.4 to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='statesegmentation',
-    version='0.0.4',
+    version='0.1.0',
     packages=['statesegmentation'],
     url='https://github.com/lgeerligs/statesegmentation',
     license='MIT',
